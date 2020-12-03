3d6a1f262132760941d1239e6e3659f39a45056f
anav238 <anavasiliu139@gmail.com>
2020-12-03 17:29:26 +0200

    tags

---
+++ project/Controllers/TagsController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using movie_recommendation.Data;
+using movie_recommendation.Entities;
+
+namespace movie_recommendation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        private readonly ITagRepository _repository;
+
+        public TagsController(ITagRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/Tags
+        [HttpGet]
+        public ActionResult<IEnumerable<Tag>> GetTags()
+        {
+            return _repository.GetAll().ToList();
+        }
+
+        // GET: api/Tags/5
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<Tag>> GetTags(int movieId)
+        {
+            var tag = _repository.GetTags(movieId).ToList();
+
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return tag;
+        }
+
+        // GET: api/Tags/1/2
+        [HttpGet("{userId}/{movieId}")]
+        public ActionResult<Tag> GetTag(int userId, int movieId)
+        {
+            var tag = _repository.GetTag(userId, movieId);
+
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return tag;
+        }
+
+
+
+        // POST: api/Tags
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public ActionResult<Tag> Create([FromBody] Tag tag)
+        {
+            try
+            {
+                _repository.Create(tag);
+            }
+            catch (Exception)
+            {
+                if (_repository.GetTag(tag.userId, tag.movieId) != null)
+                  return  Conflict();
+                else
+                    throw;
+            }
+            
+           
+            return CreatedAtAction("GetTag", new { id_1 = tag.userId, id_2 = tag.movieId }, tag);
+        }
+
+        // DELETE: api/Tags/5/5
+        
+        [HttpDelete("{userId}/{movieId}")]
+        public ActionResult<Tag> DeleteTag(int userId, int movieId)
+        {
+            var tag = _repository.GetTag(userId, movieId);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Remove(tag);
+            return tag;
+        }
+
+        
+    }
+}
--- project/Data/DataContext.cs
+++ project/Data/DataContext.cs
@@ -12,6 +12,7 @@ namespace movie_recommendation.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Friendship> Friendships { get; set; }
         public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Tag> Tags { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -19,6 +20,8 @@ namespace movie_recommendation.Data
                 .HasKey(friendship => new { friendship.UserId_1, friendship.UserId_2 });
             modelBuilder.Entity<Rating>()
                 .HasKey(rating => new { rating.userId, rating.movieId });
+            modelBuilder.Entity<Tag>()
+                .HasKey(tag => new { tag.userId, tag.movieId });
             base.OnModelCreating(modelBuilder);
         }
     }
