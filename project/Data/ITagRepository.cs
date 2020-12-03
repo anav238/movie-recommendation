9224cd26c3205dce732784c0d51b1d6ffa4f6098
anav238 <anavasiliu139@gmail.com>
2020-12-03 17:29:26 +0200

    tags

---
+++ project/Data/ITagRepository.cs
@@ -0,0 +1,15 @@
+﻿using movie_recommendation.Entities;
+using System.Collections.Generic;
+
+namespace movie_recommendation.Data
+{
+    public interface ITagRepository
+    {
+        void Create(Tag tag);
+        IEnumerable<Tag> GetAll();
+        IEnumerable<Tag> GetTags(int movieId);
+        Tag GetTag(int userId, int movieId);
+        void Remove(Tag tag);
+        void Update(Tag tag);
+    }
+}
---
+++ project/Data/TagRepository.cs
@@ -0,0 +1,52 @@
+﻿using movie_recommendation.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movie_recommendation.Data
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly DataContext _context;
+
+        public TagRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Tag> GetAll()
+        {
+            return _context.Tags.ToList();
+        }
+
+        public IEnumerable<Tag> GetTags(int movieId)
+        {
+            return _context.Tags.ToList()
+                .Where(tag => tag.movieId == movieId);
+        }
+
+        public Tag GetTag(int userId, int movieId)
+        {
+            return _context.Tags.Find(userId, movieId);
+        }
+
+        public void Create(Tag tag)
+        {
+            _context.Add(tag);
+            _context.SaveChanges();
+        }
+
+        public void Remove(Tag tag)
+        {
+            _context.Remove(tag);
+            _context.SaveChanges();
+        }
+
+        public void Update(Tag tag)
+        {
+            _context.Update(tag);
+            _context.SaveChanges();
+        }
+    }
+}
